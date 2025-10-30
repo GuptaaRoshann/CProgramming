87bb900f062bae6f8e85fae28dc50d11804512fa
GuptaaRoshann <roshang0943@gmail.com>
2025-10-30 18:48:47 +0530

    Add files via upload

---
+++ ClessonsProgram/comments/main.c
@@ -0,0 +1,15 @@
+/*name:Roshan Gupta.
+date:26/08/2025
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");//hello world
+    /*printf("hi!\n");
+    printf("Good Morning!\n");
+    printf("Good Night!\n");*/
+    return 0;
+}
